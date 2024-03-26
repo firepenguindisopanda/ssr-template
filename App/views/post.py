79b887525c59ccbc395b9732fd0778d0c3bc5cb4
Nicholas <nicosmith.smith3@gmail.com>
2024-03-26 17:07:55 +0000

    Updated: with files for post and created a new layout making use of material design

---
+++ App/views/post.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
+from flask_jwt_extended import jwt_required, current_user as jwt_current_user
+
+from.index import index_views
+
+from App.controllers import (
+    create_user,
+    get_all_users,
+    get_all_users_json,
+    jwt_required,
+    add_auth_context
+)
+
+post_views = Blueprint('post_views', __name__, template_folder='../templates')
+
+@post_views.route('/post', methods=['GET'])
+def get_shop_page():
+    
+    return render_template('post.html')
